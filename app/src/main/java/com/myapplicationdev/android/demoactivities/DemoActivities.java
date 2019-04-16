5d51742d1957347e5283f8b90ab19cca6e6a52df
NG KAI XUAN <17010489@rp.edu.sg>
2019-04-16 14:18:39 +0800

    Initial commit

---
+++ app/src/main/java/com/myapplicationdev/android/demoactivities/DemoActivities.java
@@ -0,0 +1,32 @@
+package com.myapplicationdev.android.demoactivities;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class DemoActivities extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button btnDone = findViewById(R.id.buttonDone);
+        btnDone.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditText etName = findViewById(R.id.editTextName);
+                EditText etAge = findViewById(R.id.editTextAge);
+                String[] info = {etName.getText().toString(),etAge.getText().toString()};
+                Intent i = new Intent(DemoActivities.this,DemoActivities2.class);
+
+                i.putExtra("info",info);
+
+                startActivity(i);
+            }
+        });
+    }
+}
